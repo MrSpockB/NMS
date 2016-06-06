1dde1b0c487c34cdf9ecf8c668b484f919d52121
MrSpockB <gorea_alimbico@hotmail.com>
2016-06-05 19:55:02 -0500

    Configuracion Inicial webpack
    Creación de componentes principales mas la configuración del Hot
    Reloading

---
+++ app/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component
+{
+	render()
+	{
+		return (
+			<div>
+				<p>Hello Mundo!</p>
+				<AwesomeComponent />
+			</div>
+		);
+	}
+}
+export default App;
